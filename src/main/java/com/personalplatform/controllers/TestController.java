00c979b70e6cc18cce514bb6a9975c86723d257f
AndreiSolomenko <andrsolo10@gmail.com>
2026-09-07 10:01:27 +0300

    test controller

--- src/main/java/com/personalplatform/controllers/TestController.java
+++ src/main/java/com/personalplatform/controllers/TestController.java
@@ -1,26 +1,24 @@
 package com.personalplatform.controllers;
 
-import com.personalplatform.model.TestData;
-import com.personalplatform.repos.TestDataRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    private final TestDataRepository repository;
-
-    public TestController(TestDataRepository repository) {
-        this.repository = repository;
-    }
-
     @GetMapping("/test")
-    public String test() {
-        return repository.findAll()
-                .stream()
-                .filter(item -> "test".equals(item.getKey()))
-                .map(TestData::getValue)
-                .findFirst()
-                .orElse("Key not found");
+    public String test(
+            @RequestParam(required = false) String file,
+            @RequestParam(required = false) String text
+    ) {
+
+        return """
+                FILE:
+                %s
+
+                TEXT:
+                %s
+                """.formatted(file, text);
     }
 }
